f0afc503d8df608cbb874a027378d5db3264ee98
Anibal-F <afa59@live.com.mx>
2026-02-16 16:42:45 -0700

    Stop tracking local media artifacts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      'f3b2-2806-101e-4-43dd-c9fa-83aa-3278-b209.ngrok-free.app'],
     port: 3010,
     strictPort: true
   }
